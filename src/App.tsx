9696b1f5110d78133d528137239dd78835f98462
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-16 15:40:02 +0100

    tp2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import Card from "./components/card/Card";
 import Count from "./components/count/Count";
-import Heros from "./data/Heros";
+import HerosList from "./pages/HerosList";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      {Heros.map((hero) => (
-        <Card key={hero.id} superHero={hero} />
-      ))}
+      <HerosList />
       <Count />
     </div>
   );
---
+++ src/pages/HerosList.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import Card from "../components/card/Card";
+import SuperHeros from "../data/SuperHeros";
+import SuperHero from "../models/SuperHero";
+
+const HerosList = () => {
+  const [afficher, setAfficher] = useState<SuperHero[]>([]);
+
+  useEffect(() => {
+    setAfficher(SuperHeros);
+  }, []);
+
+  return (
+    <>
+      {afficher.map((hero) => (
+        <Card key={hero.id} superHero={hero} />
+      ))}
+    </>
+  );
+};
+export default HerosList;
